2b9e6108d299b84156c06f32ce1f6b9814d76678
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-09-15 13:00:44 -0700

    🏷️ Update tag

--- tests/testthat/test_feral_cat.R
+++ tests/testthat/test_feral_cat.R
@@ -7,8 +7,8 @@ source("R/untreated_population.R")
 
 
 describe("Get version of the module", {
-  it("The version is 0.2.0", {
-    expected_version <- c("0.2.0")
+  it("The version is 0.2.1", {
+    expected_version <- c("0.2.1")
     obtained_version <- packageVersion("FeralCatEradication")
     version_are_equal <- expected_version == obtained_version
     expect_true(version_are_equal)
