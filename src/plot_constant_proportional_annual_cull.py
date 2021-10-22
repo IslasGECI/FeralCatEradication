d0bb7034286d49879afc65673c0d2a6d807aa0e1
Fernando <fernando.alvarez@islas.org.mx>
2021-10-22 12:23:48 -0700

    📝 Fix xlabel for culling model

--- src/plot_constant_proportional_annual_cull.py
+++ src/plot_constant_proportional_annual_cull.py
@@ -27,7 +27,7 @@ ax.plot(data_population["harv.prop.consist"], data_population["min.med.n"], colo
 ax.set_ylim(-0.01,1)
 ax.set_xlim(0.2,1)
 ax.legend(loc="upper left", fontsize=15)
-ax.set_xlabel("Years", size=20)
+ax.set_xlabel("Constant culling proportion", size=20)
 ax.set_ylabel(r"Proportional $N_0$", size=20)
 ax.tick_params(labelsize=15)
 plt.savefig(plot_path, dpi=300, transparent=True)
